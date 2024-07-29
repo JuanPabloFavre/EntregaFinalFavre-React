e34b832ec12b7dec670aa071f778f952522dadd9
Juan Pablo Favre <jpfavreruiz@gmail.com>
2024-07-29 15:16:24 -0300

    Se corrige el cambio de hojas para que quede abajo y se vea mejor en el celular

--- src/hooks/usePaginate.js
+++ src/hooks/usePaginate.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export const usePaginate = (data, itemsPerPage) => {
   const [currentPage, setCurrentPage] = useState(1);
--- src/pages/itemListContainer/ItemList.jsx
+++ src/pages/itemListContainer/ItemList.jsx
@@ -8,7 +8,7 @@ const ItemList = ({ items }) => {
 
     const { currentData, nextPage, prevPage, totalPages, currentPage } = usePaginate(items, 8)
     return (
-        <>
+        <div className="d-flex flex-column">
 
             <div className="d-flex flex-wrap p-3 m-3 justify-content-center " >
 
@@ -33,14 +33,20 @@ const ItemList = ({ items }) => {
 
             </div>
             <div>
-                <div className="d-flex">
 
+            </div>
+            <div className="d-flex justify-content-center align-items">
+                <div>
                     <button className="btn btn-dark p-2 m-2 " onClick={prevPage} disabled={currentPage === 1}>{<RxTrackPrevious />}</button>
+                </div>
+                <div>
                     <h4>{currentPage}/{totalPages}</h4>
+                </div>
+                <div>
                     <button className="btn btn-primary p-2 m-2" onClick={nextPage} disabled={totalPages === currentPage}>{<RxTrackNext />}</button>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
